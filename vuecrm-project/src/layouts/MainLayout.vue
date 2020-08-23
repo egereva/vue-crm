0aa4f656befcab53f145aec30ed48d87ec578f49
Marina Egereva <marina.egereva@gmail.com>
2020-08-23 13:36:46 +0300

    #9

--- vuecrm-project/src/layouts/MainLayout.vue
+++ vuecrm-project/src/layouts/MainLayout.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="app-main-layout">
-    <Navbar @clickSide="isOpen = !isOpen" />
-    <sidebar v-model="isOpen" />
+  <div>
+    <Loader v-if="loading" />
+    <div v-else class="app-main-layout">
+      <Navbar @clickSide="isOpen = !isOpen" />
+      <sidebar v-model="isOpen" />
 
-    <main class="app-content" :class="{full: !isOpen}">
-      <div class="app-page">
-        <router-view/>
+      <main class="app-content" :class="{full: !isOpen}">
+        <div class="app-page">
+          <router-view/>
+        </div>
+      </main>
+
+      <div class="fixed-action-btn">
+        <router-link class="btn-floating btn-large blue" to="/record">
+          <i class="large material-icons">add</i>
+        </router-link>
       </div>
-    </main>
-
-    <div class="fixed-action-btn">
-      <router-link class="btn-floating btn-large blue" to="/record">
-        <i class="large material-icons">add</i>
-      </router-link>
     </div>
   </div>
 </template>
@@ -24,8 +27,16 @@ import Sidebar from '@/components/app/SideBar'
 export default {
   name: 'main-layout',
   data: () => ({
-    isOpen: true
+    isOpen: true,
+    loading: true
   }),
+  async mounted () {
+    if (!Object.keys(this.$store.getters.info).length) {
+      await this.$store.dispatch('fetchInfo')
+    }
+
+    this.loading = false
+  },
   components: {
     Navbar,
     Sidebar
